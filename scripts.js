bfa3083e92130e95f65204adfc015ad292486805
KeelyRyan <keelyannryan@gmail.com>
2016-11-30 19:53:38 +0000

    cookie working

--- scripts.js
+++ scripts.js
@@ -81,11 +81,38 @@ function reflectCode(){
 }
 
 //cookie
-onload="checkCookie()"
-
 function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		var expires = "expires=" + d.toGMTString();
+		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+
+function getCookie(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i = 0; i < ca.length; i++) {
+				var c = ca[i];
+				while (c.charAt(0) == ' ') {
+						c = c.substring(1);
+				}
+				if (c.indexOf(name) == 0) {
+						return c.substring(name.length, c.length);
+				}
+		}
+		return "";
+}
+
+function checkCookie() {
+		var user=getCookie("username");
+		if (user != "") {
+	    document.getElementById("Consent").collapse = 'none';
+		} else {
+			 user = ("1");
+			 if (user != "" && user != null) {
+					 setCookie("username", user, 30);
+			 }
+		}
+}
+
+document.getElementById("Consent").addEventListener('click', checkCookie, false);
