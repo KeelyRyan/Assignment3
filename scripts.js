c3020d24064d94bdea27f941a9008c7566b1bf56
KeelyRyan <keelyannryan@gmail.com>
2016-11-30 17:05:14 +0000

    cookie

--- scripts.js
+++ scripts.js
@@ -74,7 +74,6 @@ function showRetObj (obj) {
 }
 
 
-// The function below is to reflect the contents of the javascript file back to the HTML Page
 function reflectCode(){
 	$.get('js/localStorage.js', function(data) {
 	   $('#codeExample').text(data);
@@ -82,36 +81,11 @@ function reflectCode(){
 }
 
 //cookie
-function setCookie(cname, cvalue, exdays) {
+onload="checkCookie()"
+
+function setCookie(cname,cvalue,exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
+    var expires = "expires=" + d.toGMTString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function checkCookie() {
-    var user = getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
-    } else {
-        user = prompt("Please enter your name:", "");
-        if (user != "" && user != null) {
-            setCookie("username", user, 365);
-        }
-    }
 }
